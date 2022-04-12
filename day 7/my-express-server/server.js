78a38f20cd2963aee6602ceb708648e0efbdffbf
Hector Oropesa <hectororopesa5@gmail.com>
2022-04-12 00:16:39 -0400

    First-Express-Server

---
+++ day 7/my-express-server/server.js
@@ -0,0 +1,13 @@
+// jshint esversion:6
+
+const express = require("express");
+const app = express();
+app.get("/", function (req, res) {
+  res.send("<h1>hello world!</h1>");
+});
+app.get("/about", function (req, res) {
+  res.send("<h1>about me page</h1>");
+});
+app.listen(3000, function () {
+  console.log("server started on port 3000");
+});
